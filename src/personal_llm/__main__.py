d410bb4c7619b4c33575f35949f9f65506100f4f
zhouhuaifeng <zhouhuaifeng@xunlei.com>
2026-03-28 10:58:06 +0800

    learn stream

---
+++ src/personal_llm/__main__.py
@@ -0,0 +1,52 @@
+import src.config as config
+from typing import List, Dict
+from openai import OpenAI
+
+
+class HelloAgentsLLLM:
+    def __init__(self, model: str = config.BYTEPLUS_SEED,
+                 apiKey: str = config.BYTEPLUS_API_KEY, baseUrl: str = config.BYTEPLUS_DOMAIN, timeout: int = 60):
+        self.apiKey = apiKey
+        self.model = model
+        self.baseUrl = baseUrl
+        self.timeout = timeout
+
+        if not all([self.apiKey, self.model, self.baseUrl]):
+            raise
+
+        self.client = OpenAI(api_key=apiKey, base_url=baseUrl, timeout=timeout)
+
+    def think(self, messages: List[Dict[str,str]], temperature: float = 0):
+        print(f"🧠 正在调用 {self.model} 模型...")
+        try:
+            response = self.client.chat.completions.create(
+                model=self.model,
+                messages=messages,
+                temperature=temperature,
+                stream=True,
+            )
+
+            print("✅ 大语言模型触发成功:")
+
+            collected_content = []
+            for chunk in response:
+                content = chunk.choices[0].delta.content or ""
+                print(content, end="", flush=True)
+                collected_content.append(content)
+            print()  # 在流式输出结束后换行
+            return
+
+        except Exception as e:
+            print(f"❌ 调用LLM API时发生错误: {e}")
+            return
+
+
+if __name__ == "__main__":
+    llm = HelloAgentsLLLM()
+    exampleMessages = [
+        {"role": "system", "content": "You are a helpful assistant that writes Python code."},
+        {"role": "user", "content": "介绍下你自己"}
+    ]
+    llm.think(exampleMessages)
+    print("\n\n--- 完整模型响应 ---")
+
